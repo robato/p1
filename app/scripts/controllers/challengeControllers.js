25bccb681e6e73d494bfbca3ccde6f4d835603b6
aleceiffel <bwb@truebadore.com>
2014-05-08 14:03:46 -0400

    fixed the UI to use the same data as the engine

--- app/scripts/controllers/challengeControllers.js
+++ app/scripts/controllers/challengeControllers.js
@@ -2,7 +2,7 @@ var challengeControllers = angular.module('challengeControllers', []);
 
 challengeControllers.controller('ChallengeListController', ['$scope', '$http', function($scope, $http) {
   $http.get('../docs/challenges.json').success(function(data) {
-    $scope.challenges = data;
+    $scope.challenges = data.challenges.challenge;
     $scope.challengeOrder = '@id';
   });
 }]);
--- app/scripts/controllers/rescueControllers.js
+++ app/scripts/controllers/rescueControllers.js
@@ -2,7 +2,7 @@ var rescueControllers = angular.module('rescueControllers', []);
 
 rescueControllers.controller('RescueListController', ['$scope', '$http', function($scope, $http) {
   $http.get('../docs/rescues.json').success(function(data) {
-    $scope.rescues = data;
+    $scope.rescues = data.rescues.rescue;
     $scope.orderProp = '@id';
   });
  }]);
